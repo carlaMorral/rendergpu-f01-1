df1904a9e3b834cf5426b70c8d99e15997803aaa
Albert Mir <amirmonc7@alumnes.ub.edu>
2021-05-20 22:28:01 +0200

    toon shading acabat

--- resources/fshaderToon.glsl
+++ resources/fshaderToon.glsl
@@ -207,15 +207,13 @@ vec4 blinn_phong (vec3 diffuse)
 
         vec3 rgb = hsv2rgb(hsv);
 
-        if (silhouette_emphasis_factor(V) < 0.01){
-            rgb = vec3(0,0,0);
-        }
+        //if (silhouette_emphasis_factor(V) < 0.01){
+        //    rgb = vec3(0,0,0);
+        //}
 
         cd += rgb;
 
     }
-
-    cd /= nLights;
 
     return vec4(globalAmbientLight*material.ambient + cd, 1);
 }
